64b917055acd7bcd9034d2afcc75c0cee25c76f4
SDAndreSantos <andre_santos_softeendev@outlook.com>
2022-02-14 18:03:00 -0300

    Primiero Commit

---
+++ src/main/java/br/com/softeendev/SpringReactApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.softeendev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReactApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/softeendev/SpringReactApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.softeendev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
